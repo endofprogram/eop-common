856f3222f6e7dd00fefdbc454cec263f92c4e9cf
DELL <DELL@DESKTOP-TR4OCM5>
2018-08-01 17:41:03 +0800

    id generator

---
+++ src/test/java/org/eop/common/idgene/IdGeneratorTest.java
@@ -0,0 +1,18 @@
+package org.eop.common.idgene;
+
+/**
+ * @author lixinjie
+ * @since 2018-07-19
+ */
+public class IdGeneratorTest {
+
+	public static void main(String[] args) throws Exception {
+		IdGenerator idGenerator = new CycleRadixIdGenerator();
+		//这样测试肯定会有重复Id，for循环执行的很快的
+		for (int i = 0; i < 1; i++) {
+			System.out.println(idGenerator.nextId());
+			//System.out.println(idGenerator.nextId() % 65536);
+		}
+	}
+
+}
