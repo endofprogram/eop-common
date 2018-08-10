7b00e95798762bb06e4259cfef98f56973db60a0
lixinjie1985 <lixinjie1985@126.com>
2018-08-10 15:14:18 +0800

    update

---
+++ src/main/java/org/eop/common/result/ResultCode.java
@@ -0,0 +1,28 @@
+package org.eop.common.result;
+
+/**
+ * @author lixinjie
+ * @since 2018-08-08
+ */
+public enum ResultCode {
+
+	Success(0, "成功"),
+	FAILURE(-1, "失败"),
+	ServerError(-500, "服务器程序错误");
+	
+	private int code;
+	private String desc;
+	
+	private ResultCode(int code, String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+}
