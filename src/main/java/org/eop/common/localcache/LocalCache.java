4f7be7e93a9bd022d8c47606460acdfdd58ce407
lixinjie1985 <lixinjie1985@126.com>
2018-08-17 10:32:10 +0800

    local cache by guava

---
+++ src/main/java/org/eop/common/localcache/LocalCache.java
@@ -0,0 +1,12 @@
+package org.eop.common.localcache;
+
+/**
+ * @author lixinjie
+ * @since 2018-08-08
+ */
+public interface LocalCache {
+	
+	void put(String key, Object value);
+	
+	<V> V get(String key);
+}
